1482278676ea113c78904b8518e99a9f3bec0214
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-02-24 13:18:47 +0530

    Home page complete with dropdown

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-
+document.querySelector(".dropdown").addEventListener("click",()=>{
+    document.querySelector(".dropdown-content").style.visibility="visible";
+    document.querySelector(".dropdown").style.visibility="hidden";
+})
+document.addEventListener("click", (evt)=>{
+    if(evt.target.className!="dropdown"){
+        document.querySelector(".dropdown-content").style.visibility="hidden";
+    document.querySelector(".dropdown").style.visibility="visible";
+    }
+})
 
 
 
